5775276665c3ccb8d59274ec04ac632c1568b215
ThanhThuyPNV <buithithanhthuy08022002@gmail.com>
2023-11-15 17:21:01 +0700

    Swipeable delete

--- common/ui/cardPro.jsx
+++ common/ui/cardPro.jsx
@@ -98,19 +98,10 @@
 //   },
 // });
 import React, { useState } from "react";
-import {
-  StyleSheet,
-  View,
-  ImageBackground,
-  Image,
-  Text,
-  FlatList,
-  TouchableOpacity,
-} from "react-native";
-import { Card } from "react-native-shadow-cards";
+import { StyleSheet, View, Text, FlatList, Animated,Image, TouchableOpacity } from "react-native";
+import { Swipeable } from "react-native-gesture-handler";
 import { Ionicons } from "@expo/vector-icons";
 import productsData from "../../data/data";
-// import TotalView from './totalView';
 
 function CardProduct() {
   const [data, setData] = useState(productsData);
@@ -120,16 +111,49 @@ function CardProduct() {
     setData(updatedData);
   };
 
+  const renderRightActions = (progress, dragX, onDelete) => {
+    const scale = dragX.interpolate({
+      inputRange: [-100, 0],
+      outputRange: [1, 0],
+      extrapolate: "clamp",
+    });
+    return (
+      <View style={{ backgroundColor: "#6B50F6", justifyContent: "center",width:100, height:117, borderRadius:20}}>
+        <Animated.Text
+          style={{
+            color: "white",
+            paddingHorizontal:40,
+            fontWeight: "bold",
+            transform: [{ scale }],
+          }}
+        >
+            <View style={styles.quantityControls}>
+        <TouchableOpacity onPress={onDelete}>
+          <Ionicons name="trash-outline" color={"#fff"} size={25} style={{justifyContent: "center"}} />
+        </TouchableOpacity>
+      </View>
+        </Animated.Text>
+      </View>
+    );
+  };
+
   return (
     <FlatList
       data={data}
       keyExtractor={(item) => item.id.toString()}
       renderItem={({ item }) => (
-        <CartItem product={item} onDelete={() => handleDelete(item.id)} />
+        <Swipeable
+          renderRightActions={(progress, dragX) =>
+            renderRightActions(progress, dragX, () => handleDelete(item.id))
+          }
+        >
+          <CartItem product={item} />
+        </Swipeable>
       )}
     />
   );
 }
+
 
 function CartItem({ product, onDelete }) {
   const image = product.image;
@@ -144,11 +168,6 @@ function CartItem({ product, onDelete }) {
         <Text style={styles.productName}>{product.name}</Text>
         <Text style={styles.productRestaurant}>{product.restaurant}</Text>
         <Text style={styles.productPrice}>${product.price}</Text>
-      </View>
-      <View style={styles.quantityControls}>
-        <TouchableOpacity onPress={onDelete}>
-          <Ionicons name="trash-outline" color={"#6B50F6"} size={20} />
-        </TouchableOpacity>
       </View>
     </View>
   );
